118eebf630caa7bb7f52adf395fab6ffc74de961
jeffxu71 <jeffxu71@gmail.com>
2018-05-24 10:40:36 -0400

    add tailrecQsort

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# recursion
+# Recursion
+
+Common recursive problems implemented in functional programming in Scala.
+
+# Quick Sort
+
+  implementation: normal recursion
+  implementation: tail recursion
+
+# 
---
+++ tailrecQsort.scala
@@ -0,0 +1,28 @@
+import scala.annotation.tailrec
+
+object tailrecQsort extends App {
+
+  def qsort[T](xs: List[T])(lt: (T, T) => Boolean) = {
+
+    @tailrec
+    def loop(todo: List[List[T]], done: List[T]): List[T] = todo match {
+      case Nil => done
+      case xs :: rest => xs match {
+        case Nil => loop(rest, done)
+        case x :: xrest =>
+          val (ls, rs) = (xrest partition(lt(x, _)))
+          if (ls.isEmpty) {
+            if (rs.isEmpty) loop(rest, x :: done)
+            else loop(rs :: rest, x :: done)
+          }
+          else loop (ls :: List(x) :: rs :: rest, done)
+      }
+    }
+    loop(List(xs), Nil)
+  }
+
+  val l = List[Int](6,12,3,9,3,5,1)
+  val z = qsort(l)((x, y) => if (x<y) true else false)
+
+  println(z)
+}
